567e1362a948dbb1dd5d88072a915f5809c5d23b
karl marx <5042021062karlmarx@gmail.com>
2020-11-10 06:02:11 -0500

    migrate to gcp

--- src/main/java/com/karlmarxindustries/grindripsum/dao/DaoImpl.java
+++ src/main/java/com/karlmarxindustries/grindripsum/dao/DaoImpl.java
@@ -11,6 +11,8 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
 import java.util.Scanner;
+
+
 @Repository
 public class DaoImpl {
     private final String PHRASE_LIST = "list.txt";
